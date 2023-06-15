30b3ce2dd42f9ed169ec797b0350e0a5f1c736b2
Dahan Imad <imaddahan44@gmail.com>
2023-06-15 16:42:25 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
@@ -7,7 +6,6 @@
  *
  * return: 0 always
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

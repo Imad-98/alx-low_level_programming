97ee234aaa911cd2272906c8233fd3b1c167c27a
Dahan Imad <imaddahan44@gmail.com>
2023-06-15 16:40:11 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description a c program
+ *
+ * return: 0 always
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
